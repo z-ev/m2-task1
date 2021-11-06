46e6a62a2e034e02453876230493d214dcd4bc8a
evgeniizab <evgenii@zabairachnyi.com>
2021-11-07 01:51:26 +0400

    build: add registration.php and module.xml

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © 2021 Eugene e@zab.bz
+ * See LICENSE for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Magestack_Task1',
+    __DIR__
+);
